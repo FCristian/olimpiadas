bb1d71e5fa39c2ef124ca326b228c550da73981d
FCristian <FCristian@users.noreply.github.com>
2024-09-08 23:54:15 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Uma das maiores ginastas brasileiras de todos os tempos, conquistou o coração dos brasileiros com suas performances impecáveis e sua determinação inabalável.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginasta ouro"
+    },
+    {
+        titulo: "Rayssa Leal",
+        descricao: "A Fadinha! Skatista profissional brasileira, mais jovem medalhista olímpica do país. Conquistou o mundo com seu estilo único e carisma, inspirando jovens skatistas.",
+        link: "https://pt.wikipedia.org/wiki/Raissa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Judoca brasileira, campeã olímpica de judô. Conhecida por sua técnica impecável e sua força de vontade, Beatriz é um exemplo de superação e dedicação.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoka"
+    }
+];
